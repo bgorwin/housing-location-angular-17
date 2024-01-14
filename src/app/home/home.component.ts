491ed5d75a8e699e23bd49c76c01509c745db714
Brent Gorwin <brent.gorwin@gmail.com>
2024-01-14 16:03:51 -0600

    Added the Housing Service that stores all the data array for the cards. The home component is displaying them using ngFor, but might need to update that to the new for loop in Angular.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -12,11 +15,22 @@ import { CommonModule } from '@angular/common';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-housing-location 
+        *ngFor="let housingLocation of housingLocationList" 
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
   `,
   styleUrls: [
-    '../home.component.css' 
+    './home.component.css' 
   ],
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
