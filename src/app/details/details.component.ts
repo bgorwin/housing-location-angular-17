0db3bf5dbd5ba288657017df0abd58a6e4297fa7
Brent Gorwin <brent.gorwin@gmail.com>
2024-01-20 11:18:42 -0600

    Added routes to the navigation.

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DetailsComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@
  */
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
+import {provideRouter} from '@angular/router';
+import routeConfig from './app/routes';
 
-bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
-  console.error(err),
-);
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
